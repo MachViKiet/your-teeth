5c9514f294411d84e3bdd8f264a11a8bdce352f3
Mạch Vĩ Kiệt <130826069+MachViKiet@users.noreply.github.com>
2023-10-17 12:47:03 +0700

    update : app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/Home" element={<Dashboard/>} />
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/Home" element={<Home/>} />
         <Route path="/Register" element={<Register/>} />
         <Route path="/LogIn" element={<LogIn/>} />
       </Routes>
